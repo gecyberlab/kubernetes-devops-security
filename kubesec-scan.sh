cd85f340100b7074a6954a7b16c216eb11e7321a
root <root@User-PC.localdomain>
2021-12-18 22:25:45 +0200

    added k8s scans

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -6,7 +6,7 @@
 
 # using kubesec v2 api
 scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r ) 
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan) 
 scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score ) 
 
 
@@ -19,12 +19,12 @@ scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https:/
     # Kubesec scan result processing
     # echo "Scan Score : $scan_score"
 
-	if [[ "${scan_score}" -ge 5 ]]; then
+	if [[ "${scan_score}" -ge 1 ]]; then
 	    echo "Score is $scan_score"
 	    echo "Kubesec Scan $scan_message"
 	else
 	    echo "Score is $scan_score, which is less than or equal to 5."
-	    echo "Scanning Kubernetes Resource has Failed"
+	    echo "Scanning Kubernetes Resource has Failed\n\r$scan_message"
 	    exit 1;
 	fi;
 
